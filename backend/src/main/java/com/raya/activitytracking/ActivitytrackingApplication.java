cbe4b4ddf7113714c0c787adb4abcfefa31bb6e0
BasemEmad <basem.smsm24@gmail.com>
2026-08-12 02:03:54 +0300

    chore: initialize backend structure with Spring Boot and Docker setup

---
+++ backend/src/main/java/com/raya/activitytracking/ActivitytrackingApplication.java
@@ -0,0 +1,13 @@
+package com.raya.activitytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitytrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitytrackingApplication.class, args);
+    }
+
+}
